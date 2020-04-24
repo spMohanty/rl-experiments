95936156565ef3f77edd1fffa702361809545567
nilabha <nilabha2007@gmail.com>
2020-04-24 19:13:37 +0530

    Changes to include eager true support by moving all numpy code to tensorflow. Further all dtypes are standardised to tf.float32 and tf.int32

--- models/param_action_model.py
+++ models/param_action_model.py
@@ -33,42 +33,65 @@ class ParamActionModel(TFModelV2):
 
     def forward(self, input_dict, state, seq_lens):
         # Extract the available actions tensor from the observation.
+        float_tf_type = tf.float32
         action_mask = input_dict["obs"]["action_mask"]
 
         # Compute the predicted action embedding
         action_embed_orig, _ = self.action_embed_model({
             "obs": input_dict["obs"]["rogi"]
         })
+        action_embed_orig = tf.cast(action_embed_orig, float_tf_type)
+        action_mask = tf.cast(action_mask, float_tf_type)
+
         action_dims = self.action_space.nvec
         num_outputs = sum(action_dims)
         width = action_dims[1]
 
         action_embed = tf.squeeze(action_embed_orig)
-        action_mask_actual = np.array([1.] * num_outputs)
+        action_mask_actual = tf.constant([1.]*num_outputs,
+                                         dtype=float_tf_type)
+
         action_mask = tf.squeeze(action_mask)
         n_action_types = len(ActionType)
-        end_x = width + n_action_types
+        end_x = n_action_types + width
         action_type = tf.argmax(action_embed[:n_action_types], axis=-1)
         cell_x = tf.argmax(action_embed[n_action_types:end_x], axis=-1)
         cell_y = tf.argmax(action_embed[end_x:], axis=-1)
 
         action_mask_value = action_mask[cell_x, cell_y]
 
-        if(int(action_type) == 1):
-            # Change vaccination to Step
-            cell_x_idx = n_action_types + cell_x
-            cell_y_idx = n_action_types + width + cell_y
-            if int(action_mask_value) == 0:
-                action_mask_actual[1] = 0  # Disable Vaccinate
-                action_mask_actual[cell_x_idx] = 0  # Mask Cell X
-                action_mask_actual[cell_y_idx] = 0  # Mask Cell Y
+        cell_x_idx = tf.add(tf.constant([n_action_types],
+                            dtype=cell_x.dtype), cell_x)
+        cell_y_idx = tf.add(tf.constant([end_x], dtype=cell_y.dtype), cell_y)
+        vaccinate_idx = tf.constant([ActionType.VACCINATE.value],
+                                    dtype=cell_x.dtype)
+
+        vaccinate_idx = tf.cast(vaccinate_idx, tf.int32)
+        cell_x_idx = tf.cast(cell_x_idx, tf.int32)
+        cell_y_idx = tf.cast(cell_y_idx, tf.int32)
+        indices = tf.stack(values=[vaccinate_idx,
+                                   cell_x_idx, cell_y_idx], axis=0)
+        updates = tf.constant([1, 1, 1])
+        shape = tf.constant([num_outputs])
+        masker = tf.scatter_nd(indices, updates, shape)
+
+        masker = tf.cast(masker, float_tf_type)
+
+        action_mask_vaccinate = tf.cond(tf.equal(action_mask_value, 1),
+                                        lambda: action_mask_actual,
+                                        lambda: action_mask_actual - masker)
+
+        action_mask_actual = tf.cond(tf.equal(action_type,
+                                     ActionType.VACCINATE.value),
+                                     lambda: action_mask_vaccinate,
+                                     lambda: action_mask_actual)
 
         action_logits = action_embed_orig
 
-        # Mask out invalid actions (use tf.float32.min for stability)
-        action_mask_actual = tf.constant(action_mask_actual, dtype=tf.float32)
+        # Mask out invalid actions (use tf.float32.min for stability
         inf_mask = tf.maximum(tf.log(action_mask_actual),
-                              tf.float32.min)
+                              float_tf_type.min)
+
         inf_mask = tf.expand_dims(inf_mask, 0)
 
         action_logits_updated = action_logits + inf_mask
